cd51ce71852cc277bd9aad5af6971f7e5bda1912
hameelhusnain <husnainhameel3@gmail.com>
2024-12-29 00:46:44 +0500

    edit index.css file and add some new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import Hero from './components/Hero.jsx'
 
 const App = () => {
   return (
-    <main>
-      <h1 className='text-5xl text-violet-300 font-bold font-circular-web '>hameelhusnain</h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
     </main>
   )
 }
